b15a53b7a369fc434c561b5c9ac7c423a2641882
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-19 08:40:39 -0500

    Completes 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    const newArr=[];
+    for(let i=0;i<arr.length;i++){
+        let add=true;
+        for(let j=0;j<args.length;j++){
+            if(arr[i]===args[j]){
+                add=false;
+                break;
+            }
+        }
+        if(add){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
