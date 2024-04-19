03fd1ea144aa19fb5fdf3bb8e5ffd7bdfbcb568f
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-19 09:15:47 -0500

    completes 7th

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp=(temp-32)*5/9;
+  temp*=10;
+  temp=Math.round(temp);
+  temp/=10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp=temp*9/5+32;
+  temp*=10;
+  temp=Math.round(temp);
+  temp/=10;
+  return temp;
 };
 
 // Do not edit below this line
