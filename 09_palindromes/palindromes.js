f8c97e8883c7d590c5a0c1756b1fa2aa24f5f915
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 14:36:51 -0500

    Completes 9th

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let str="";
+    for(let i=0;i<s.length;i++){
+        if(s[i]!='.'&&s[i]!=','&&s[i]!=' '&&s[i]!='!'){
+            str+=s.substring(i,i+1);
+        }
+    }
+    let t=true;
+    str=str.toLowerCase();
+    for(let i=0;i<str.length/2;i++){
+        if(str[i]!=str[str.length-1-i]){
+            t=false;
+        }
+    }
+    return t;
 };
 
 // Do not edit below this line
