cedc3585ff123ef7e1236e1e6423c77f628c1e3d
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 15:06:13 -0500

    Completes final excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    let name=""; let max=0;
+    for(let i=0;i<peoples.length;i++){
+        let age=0;
+        if(peoples[i].yearOfDeath==undefined){
+            age=2024-peoples[i].yearOfBirth;
+        }
+        else{
+            age=peoples[i].yearOfDeath-peoples[i].yearOfBirth;
+        }
+        if(age>max){
+            name=peoples[i];
+            max=age;
+        }
+    }
+    return name;
 };
 
 // Do not edit below this line
