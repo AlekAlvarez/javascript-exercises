2231cd3e0ca5c9b5eae5e9a88c6af5ab526af8f6
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 14:05:06 -0500

    Completes eight excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum=0;
+  for(let i=0;i<a.length;i++){
+    sum+=a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product=1;
+  for(let i=0;i<a.length;i++){
+    product*=a[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let answer=1;
+  for(let i=1;i<=power;i++){
+    answer*=base;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer=1;
+  for(let i=1;i<=a;i++){
+    answer*=i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
