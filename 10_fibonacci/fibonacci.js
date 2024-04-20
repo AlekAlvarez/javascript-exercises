48d98576a977be4198c89cbd2c6e190041dcbe80
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 14:43:40 -0500

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index=index*1;
+    if(index<0)return "OOPS";
+    if(index==0)return 0;
+    let prev=0;let current=1;
+    for(let i=1;i<index;i++){
+        let temp=current;
+        current+=prev;
+        prev=temp;
+    }
+    return current;
 };
 
 // Do not edit below this line
